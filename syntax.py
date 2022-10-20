16638f065be1eb6388c1ad4b03d12453eb83e7a7
ElizabethJ0 <116313967+ElizabethJ0@users.noreply.github.com>
2022-10-20 22:00:54 +0000

    The code

---
+++ syntax.py
@@ -0,0 +1,12 @@
+#print("Hello World")
+print("ELizabeth")
+print("John")
+print("Roosevlt high school has a marching band")
+y=5
+print(y)
+a="Elizabeth"
+t=5
+print(y*t)
+print (a)
+print("hi\ngoodbye")
+#\n skips a line
