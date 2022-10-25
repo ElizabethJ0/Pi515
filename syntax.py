85d6cdfe7638bfb7c1953a5e8bf96fc3ba50ea32
ElizabethJ0 <116313967+ElizabethJ0@users.noreply.github.com>
2022-10-25 21:59:47 +0000

    10/25

--- syntax.py
+++ syntax.py
@@ -10,3 +10,27 @@ print(y*t)
 print (a)
 print("hi\ngoodbye")
 #\n skips a line
+x=3
+print(x)
+print(x+t)
+print(x*t+x-y)
+print(str(x)+"Elizabeth")
+print(str(x-y*t)+"John")
+h="Hi"
+print(str(x)+a+h)
+v1,v2,v3="red","blue","white"
+print(v1)
+print(str(x)+v1)
+print(v1,v2,v3)
+colors=["red","blue","white"]
+print(colors)
+b, c, d = colors
+print(d)
+print(d+" "+c)
+color=["purple",1]
+p, i =color
+print(p)
+print(i)
+n=2
+print(x+n/y)
+print(n,x)
