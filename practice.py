9be1852499c99921a91ec1f34055e30923e8798a
ElizabethJ0 <116313967+ElizabethJ0@users.noreply.github.com>
2022-12-08 23:01:20 +0000

    12/8

--- practice.py
+++ practice.py
@@ -1,31 +1,47 @@
-min = 10000
-max = -10000
-numList = [1,12,8,5,20,80,40,3,6,2000]
-#numList=[0,0,0,0]
-for x in numList:
- # if(min<x):
-    #print (x)
-  if (max<x):
-    #print(x)
-    max = x
-  if(min>x):
-    #print (x) 
- # elif(min>x):
-    #print (x, "This is the smallest so far")
-    min = x
+# min = 10000
+# max = -10000
+# numList = [1,12,8,5,20,80,40,3,6,2000]
+# #numList=[0,0,0,0]
+# for x in numList:
+#  # if(min<x):
+#     #print (x)
+#   if (max<x):
+#     #print(x)
+#     max = x
+#   if(min>x):
+#     #print (x) 
+#  # elif(min>x):
+#     #print (x, "This is the smallest so far")
+#     min = x
 
-print ("This is the min:", min, "and This is the max:", max)
-#print (max, "This is the max")
+# print ("This is the min:", min, "and This is the max:", max)
+# #print (max, "This is the max")
 
-flavors= ["Chocolate", "Vanilla", "Cookie Dough", "Strawberry", "Cookies and cream"]
-flavors2=["Chocolate", "Vanilla","mint chocolate chip ", "Strawberry", "cookies and cream"]
 
-flavors.insert(2,"mint chocolate chip")
-print(flavors)
-flavors.remove("Strawberry")
-print(flavors)
+# flavors= ["Chocolate", "Vanilla", "Cookie Dough", "Strawberry", "Cookies and cream"]
+# flavors2=["Chocolate", "Vanilla","mint chocolate chip ", "Strawberry", "cookies and cream"]
+
+# flavors.insert(2,"mint chocolate chip")
+# print(flavors)
+# flavors.remove("Strawberry")
+# print(flavors)
               
-print("Vanilla"in flavors)
-print("Vanilla"in flavors2)
-print("Cookie Dough" in flavors)
-print("Cookie Dough" in flavors2)
+# print("Vanilla"in flavors)
+# print("Vanilla"in flavors2)
+# print("Cookie Dough" in flavors)
+# print("Cookie Dough" in flavors2)
+
+
+# countries = ["Italy", "England", "France", "Spain", "Canada"]
+
+# for country in countries:
+#   if country != "England":
+#     print(country)
+#   if country == "England":
+#     print ("This is a country in Europe: " + country) 
+
+
+
+for country in "countries":
+  for letter in country:
+    print (letter)
